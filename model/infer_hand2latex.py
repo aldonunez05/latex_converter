1456c96b3e34c0c09c72cb1f4e8e8c69f99ac9aa
Aldo Nunez <aldo.nunez.jr@gmail.com>
2025-11-07 22:56:37 -0800

    added poisitonal encoder

--- model/infer_hand2latex.py
+++ model/infer_hand2latex.py
@@ -47,3 +47,19 @@ class CNNEncoder(nn.Module):
 		B, D, Hf, Wf = feat.shape
 		seq = feat.permute(0, 2, 3, 1).contiguous().view(B, Hf*Wf, D)
 		return seq
+
+# Positional encoder
+class PositionalEncoding(nn.Module):
+	def __init__(self, d_model, max_len=10000):
+		super().__init__()
+		pe = torch.zeros(max_len, d_model)
+		position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+		div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+		pe[:, 0::2] = torch.sin(position * div_term)
+		pe[:, 1::2] = torch.cos(position * div_term)
+		self.register_buffer('pe', pe)
+
+	def forward(self, x);
+		T = x.size(1)
+		return x + self.pe[:T].unsqueeze(0)
+
