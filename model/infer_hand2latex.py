1ce6861e801d4651eae903e177b8cf5a0da0a4b0
Aldo Nunez <aldo.nunez.jr@gmail.com>
2025-11-11 15:13:50 -0800

    image2latex class

--- model/infer_hand2latex.py
+++ model/infer_hand2latex.py
@@ -62,4 +62,32 @@ class PositionalEncoding(nn.Module):
 	def forward(self, x);
 		T = x.size(1)
 		return x + self.pe[:T].unsqueeze(0)
+# Img to Latex
+class Image2Latex(nn.Module):
+	def __init__(self, vocab_size, d_model=256, nhead=8, num_layers=3, pad_idx=0):
+		super().__init__()
+		self.encoder = CNNEncoder(out_dim=d_model)
+		self.pos_enc = PositionalEncoding(d_model)
+		self.tok_emb = nn.Embedding(vocab_size, d_model, padding_idx=pad_idx)
+		decode_layer = nn.TransformerDecoder(decoder_layer, num_layers=num_layers)
+		self.output_proj = nn.Linear(d_model, vocab_size)
+		self.d_model = d_model
 
+	def encode_images(self, images):
+		mem = self.encoder(images)  # [B, S, D]
+		mem = self.pos_enc(mem)
+		# transformer expects seq first
+		return mem.permute(1, 0 , 2) # [S, B, D]
+
+	def decode_step(self, tgt_ids, memory, tgt_mask=None):
+		# tgt_ids shape [B, T}
+		tgt = self.tok_emb(tgt_ids) * math.sqrt(self.d_model)
+		tgt = self.pos_enc(tgt)
+		tgt = tgt.permute(1, 0, 2) # [T, B, D]
+		if tgt_mask is None:
+			T = tgt.size(0)
+			tgt_mask = nn.Transformer.generate_square_subsequent_mask(T).to(tgt.device)
+		out = self.transformer_decoder(tgt, memory, tgt_mask=tgt_mask)
+		out = out.permute(1, 0, 2) # [B,T,D]
+		logits = self.output_proj(out) # [B, T, V]
+		return logits
